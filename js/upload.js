de4bcbc8c45a16d390080a5e1a0ee38c16bd538a
smallis <smallis_89@hotmail.com>
2020-04-29 10:52:03 -0400

    Add files via upload

--- js/upload.js
+++ js/upload.js
@@ -42,8 +42,10 @@ function processDataAsObj (csv) {
   var keys = header.split(csvDelimiter);
 
   let isValid = true;
+  let hasLocation = true;
   const requiredKeys = ["customerNumber", "customerName",
-    "deliveryDetails", "Latitude", "Longitude"];
+    "deliveryDetails"];
+  const locationKeys =["Latitude", "Longitude", "Link"];
   while (allTextLines.length) {
     var arr = allTextLines.shift().split(csvDelimiter);
 	console.log(`allTextLines:${arr}`);
@@ -52,7 +54,10 @@ function processDataAsObj (csv) {
       return;
     }
     var obj = {};
-    isValid = requiredKeys.every(reqKey => keys.includes(reqKey))
+    isValid = requiredKeys.every(reqKey => keys.includes(reqKey));
+    hasLocation = locationKeys.some(reqKey => keys.includes(reqKey));
+    console.log(`isValid:${isValid}`);
+    console.log(`hasLocation:${hasLocation}`);
     for (var i = 0; i < keys.length; i++) {
       obj[keys[i]] = arr[i];
     }
@@ -67,10 +72,18 @@ function processDataAsObj (csv) {
   const isSquareArea = 0;
   const showOnMyGeoTabByDefault = 1;
   const apiCalls = lines.map(line => {
-    const { customerNumber, customerName, deliveryDetails, Latitude, Longitude } = line;
+  const { customerNumber, customerName, deliveryDetails, Latitude, Longitude, Link } = line;
+    
+  var Lat = 0;
+  var Long = 0;
+	
+    var coords = findLocation (Latitude, Longitude, Link);
+    Lat = coords[0];
+    Long = coords[1];
+	
     const { zonePoints } = generateZonePoints({
-      y: Number(Latitude),
-      x: Number(Longitude)
+      y: Number(Lat),
+      x: Number(Long)
     },
       isSquareArea,
       areaRadius);
@@ -80,6 +93,49 @@ function processDataAsObj (csv) {
     .then(x => alert("File has been processed. All customers added"))
     .catch(err => console.error("An error occured:", err));
 }
+
+function findLocation (Latitude, Longitude, Link){
+
+  const constantsSpecified = Latitude !== "" && Longitude !== "";
+	const linkSpecified = Link !== "";
+	var coord = [];
+	
+	if (constantsSpecified || linkSpecified) 
+	{
+     
+    if (linkSpecified)
+    {
+      coord = parseLink(Link);
+    }
+    if(constantsSpecified) 
+    {
+      coord.push(Latitude);
+      coord.push(Longitude);
+    }
+  }
+      
+    console.log(`coord:${coord}`);
+    return coord;
+}
+
+function parseLink(link)
+{
+	var lk = link.split("&");
+	var str = lk[0];
+	var res = str.split("=");
+	var parse = res[1];
+	var check = parse.search("%2C");
+	if(check >= 0)
+	{
+		var coord = parse.split("%2C");
+	}
+	else
+	{
+		var coord = parse.split(",");
+	}
+	return coord;
+}
+
 
 function errorHandler (evt) {
   if (evt.target.error.name == "NotReadableError") {
